f5cb95f9864660440089f94fbaac291ca59a5824
ManuelMonroy91 <Manuel.Monroy@jala.university>
2025-01-08 16:55:41 -0400

    Initial DataBase Commit

---
+++ src/DB/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DB
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Users> Users { get; set; }
+        public DbSet<Tokens> Tokens { get; set; }
+        public DbSet<Courses> Courses { get; set; }
+        public DbSet<StudentCourse> StudentCourses { get; set; }
+        public DbSet<ComponentVariable> ComponentVariables { get; set; }
+        public DbSet<Polls> Polls { get; set; }
+        public DbSet<Answers> Answers { get; set; }
+        public DbSet<Students> Students { get; set; }
+        public DbSet<StudentDetails> StudentDetails { get; set; }
+        public DbSet<Rules> Rules { get; set; }
+        public DbSet<RiskVariables> RiskVariables { get; set; }
+        public DbSet<RiskPopulation> RiskPopulation { get; set; }
+        public DbSet<Report> Reports { get; set; }
+
+    }
+}
---
+++ src/DB/Rules.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB
+{
+    public class Rules
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        public int ReportId { get; set; }
+
+        [ForeignKey("ReportId")]
+        public virtual Report Report { get; set; }
+
+        public int? ComponentVariableId { get; set; }
+
+        [ForeignKey("ComponentVariableId")]
+        public virtual ComponentVariable ComponentVariable { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+
+        [DataType(DataType.DateTime)]
+        public DateTime ModifiedDate { get; set; } = DateTime.UtcNow;
+    }
+}
