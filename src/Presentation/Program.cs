f1ff5810b8238feb92234a27597abcf8091dd011
Hap Rojas <adrian.rojas@abe.bo>
2025-01-09 14:22:56 -0400

    added cors

--- src/Presentation/Program.cs
+++ src/Presentation/Program.cs
@@ -4,8 +4,17 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddCors(o => {
+    o.AddPolicy("MyPolicy", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+    });
+});
 var app = builder.Build();
+
+app.UseCors("MyPolicy");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
