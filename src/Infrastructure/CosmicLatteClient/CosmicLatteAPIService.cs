195e87589a37d507a88bd829e32f79b12445ba01
henry-bautista-benchmark <henry@benchmarkemail.com>
2025-01-10 10:24:19 -0400

    Remove un used imports

--- src/Infrastructure/CosmicLatteClient/CosmicLatteAPIService.cs
+++ src/Infrastructure/CosmicLatteClient/CosmicLatteAPIService.cs
@@ -1,12 +1,6 @@
 ﻿using Entities;
 using Microsoft.Extensions.Configuration;
 using Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Infrastructure.CosmicLatteClient.CosmicLatteClient
 {
