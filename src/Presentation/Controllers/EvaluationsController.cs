d8bb3ed3f731b5b695cb6508b2f3fee151950bbf
henry-bautista-benchmark <henry@benchmarkemail.com>
2025-01-10 10:17:25 -0400

    Remove comments

--- src/Presentation/Controllers/EvaluationsController.cs
+++ src/Presentation/Controllers/EvaluationsController.cs
@@ -20,29 +20,6 @@ namespace Presentation.Controllers
         {
             return Ok(await _cosmicLatteService.CosmicApiIsHealthy());
         }
-
-        /*
-
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Domain.Entities.Evaluation>> GetEvaluationById(string id)
-        {
-            ActionResult<Domain.Entities.Evaluation> Evaluation = await _cosmicLatteService.GetEvaluationById(id);
-            if (Evaluation == null) return NotFound($"No se encontró evaluación con id {id}");
-            return Ok(Evaluation);
-        }
-
-        [HttpGet]
-        public async Task<ActionResult<List<Domain.Entities.Evaluation>>> GetEvaluations(
-            [FromQuery] string name = null,
-            [FromQuery] string startDate = null, // yyyy or yyyy-mm or yyyy-mm-dd
-            [FromQuery] string endDate = null // yyyy or yyyy-mm or yyyy-mm-dd
-            )
-        {
-            return Ok(await _cosmicLatteService.GetEvaluations(name, startDate, endDate));
-        }
-        */
-
-
     }
 }
 
