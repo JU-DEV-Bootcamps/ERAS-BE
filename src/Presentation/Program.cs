0ad0f4a475c6ac7e15b9917ece8fff1ddaa91cdb
Hap Rojas <adrian.rojas@abe.bo>
2025-01-17 12:16:20 -0400

    uncomment migrations

--- src/Presentation/Program.cs
+++ src/Presentation/Program.cs
@@ -71,7 +71,7 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    //dbContext.Database.Migrate();
+    dbContext.Database.Migrate();
 }
 
 // Enable CORS
