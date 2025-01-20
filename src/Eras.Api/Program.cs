b58302b173dcf2076f2b1d41deffd41c35f93ca6
henry-bautista-benchmark <henry@benchmarkemail.com>
2025-01-20 16:10:58 -0400

    Move project into a new solution

---
+++ src/Eras.Api/Program.cs
@@ -0,0 +1,91 @@
+using Eras.Application.Services;
+using Eras.Infrastructure.External.CosmicLatteClient;
+using Eras.Infrastructure.External.KeycloakClient;
+using Eras.Infrastructure.Persistence.PostgreSQL;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddHttpClient();
+builder.Services.AddSingleton<ICosmicLatteAPIService, CosmicLatteAPIService>();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(o =>
+{
+    o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidIssuer = $"{builder.Configuration["Keycloak:BaseUrl"]}/realms/{builder.Configuration["Keycloak:Realm"]}",
+
+        ValidateAudience = true,
+        ValidAudience = "account",
+
+        ValidateIssuerSigningKey = true,
+        ValidateLifetime = false,
+
+        IssuerSigningKeyResolver = (token, securityToken, kid, parameters) =>
+        {
+            var client = new HttpClient();
+            var keyUri = $"{parameters.ValidIssuer}/protocol/openid-connect/certs";
+            var response = client.GetAsync(keyUri).Result;
+            var keys = new JsonWebKeySet(response.Content.ReadAsStringAsync().Result);
+
+            return keys.GetSigningKeys();
+        }
+    };
+
+    options.RequireHttpsMetadata = false; // Only in develop environment
+    options.SaveToken = true;
+});
+
+builder.Services.AddScoped<KeycloakAuthService>();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("ErasConnection")));
+
+builder.Services.AddCors(o =>
+{
+    o.AddPolicy("CORSPolicy", policy =>
+    {
+        string allowedHosts = builder.Configuration["AllowedHosts"] ?? "*";
+        policy.WithOrigins(allowedHosts)
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+// Apply database migrations
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+}
+
+// Enable CORS
+app.UseCors("CORSPolicy");
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+app.Run();
---
+++ src/Eras.Infrastructure/External/KeyCloakClient/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Eras.Infrastructure.External.KeycloakClient
+{
+    public class LoginRequest
+    {
+        required public string Username { get; set; }
+        required public string Password { get; set; }
+    }
+}
