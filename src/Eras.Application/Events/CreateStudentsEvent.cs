9d442a85000f4f553081fd80c7881a5118d75f66
Ram <ramiro.vargas@jala.university>
2025-02-04 17:24:29 -0400

    feat(application): add basic structure for mediator for import student with csv

---
+++ src/Eras.Application/Events/CreateStudentsEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Eras.Application.DTOs;
+using Eras.Domain.Entities;
+using MediatR;
+
+namespace Eras.Application.Events
+{
+    public class CreateStudentsEvent : IRequest<bool>
+    {
+        public Student student;
+        public CreateStudentsEvent(Student student) {
+            this.student = student;
+        }
+    }
+}
---
+++ src/Eras.Application/Handlers/CreateStudentsHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Eras.Application.DTOs;
+using Eras.Application.Events;
+using Eras.Domain.Repositories;
+using MediatR;
+
+namespace Eras.Application.Handlers
+{
+    public class CreateStudentsHandler : IRequestHandler<CreateStudentsEvent, bool>
+    {
+
+        private IStudentRepository _studentRepository;
+
+        public CreateStudentsHandler(IStudentRepository studentRepository)
+        {
+            _studentRepository = studentRepository;
+        }
+
+        public Task<bool> Handle(CreateStudentsEvent request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                _studentRepository.SaveAsync(request.student);
+                return Task.FromResult(true);
+            }
+            catch (Exception ex)
+            {
+                return Task.FromResult(false);
+            }
+        }
+    }
+}
