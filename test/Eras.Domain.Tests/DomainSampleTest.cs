d1a09e8351156dca56fe74fa7245eef0447c107d
henry-bautista <henry.bautista@jalasoft.com>
2025-02-05 15:16:13 -0400

    Fix test

--- test/Eras.Domain.Tests/DomainSampleTest.cs
+++ test/Eras.Domain.Tests/DomainSampleTest.cs
@@ -1,4 +1,5 @@
-﻿using Eras.Infrastructure.Persistence.PostgreSQL;
+﻿using Eras.Domain.Common;
+using Eras.Domain.Entities;
 using Moq;
 
 namespace Eras.Domain.Tests
@@ -11,10 +12,18 @@ namespace Eras.Domain.Tests
             // Arrange
             var mockRepo = new Mock<IPollRepositorySample>();
             //-DB Data example
-            var expectedPolls = new List<PollsEntity>
+            var expectedPolls = new List<Poll>
             {
-                new PollsEntity { Id = 1, Name = "Poll 1", CreatedDate = DateTime.UtcNow, ModifiedDate = DateTime.UtcNow },
-                new PollsEntity { Id = 2, Name = "Poll 2", CreatedDate = DateTime.UtcNow, ModifiedDate = DateTime.UtcNow }
+                new Poll { 
+                    Id = 1,
+                    Name = "Poll 1",
+                    Audit = new AuditInfo { CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now }
+                },
+                new Poll { 
+                    Id = 2,
+                    Name = "Poll 2",
+                    Audit = new AuditInfo { CreatedAt = DateTime.Now, ModifiedAt = DateTime.Now }
+                }
             };
 
             mockRepo.Setup(repo => repo.GetAllPollsAsync()).ReturnsAsync(expectedPolls);
