3aeb5bd180b1ee7ea6061e89c1088341e0af90ce
Israel <israelquisbert.dev@gmail.com>
2025-02-06 08:53:59 -0400

    feat(Application): Added the handlers for Answers and Polls, there is a conflict with the mapper

---
+++ src/Eras.Application/Features/Answers/Commands/CreateAnswer/CreateAnswerCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Eras.Application.Contracts.Persistence;
+using Eras.Application.Utils;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Eras.Application.Features.Answers.Commands.CreateAnswer
+{
+    public class CreateAnswerCommandHandler : IRequestHandler<CreateAnswerCommand, BaseResponse>
+    {
+        private readonly IAnswerRepository _answerRepository;
+        private readonly ILogger<CreateAnswerCommandHandler> _logger;
+
+        public CreateAnswerCommandHandler(IAnswerRepository answerRepository, ILogger<CreateAnswerCommandHandler> logger)
+        {
+            _answerRepository = answerRepository;
+            _logger = logger;
+        }
+
+        public async Task<BaseResponse> Handle(CreateAnswerCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var poll = request.answer.ToAnswer();
+                await _answerRepository.AddAsync(poll);
+                return new BaseResponse(true);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred creating the poll: " + request.answer.GetHashCode);
+                return new BaseResponse(false);
+            }
+        }
+    }
+}
---
+++ src/Eras.Application/Features/Polls/Commands/CreatePoll/CreatePollCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Eras.Application.Dtos;
+using Eras.Application.Utils;
+using Eras.Domain.Entities;
+using MediatR;
+
+namespace Eras.Application.Features.Polls.Commands.CreatePoll
+{
+    public class CreatePollCommand : IRequest<BaseResponse>
+    {
+        public PollDTO poll;
+    }
+}
