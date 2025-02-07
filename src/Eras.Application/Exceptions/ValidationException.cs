d80c45b6ec37aac6501246f4b76b668dd4ee88be
Israel <israelquisbert.dev@gmail.com>
2025-02-07 10:31:40 -0400

    feat(Application): Added exceptions and validations

---
+++ src/Eras.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eras.Application.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public List<string> ValidationErrors { get; set; }
+        public ValidationException(ValidationResult validationResult)
+        {
+            ValidationErrors = new List<string>();
+            foreach(var validationError in validationResult.Errors)
+            {
+
+            }
+        }
+    }
+}
