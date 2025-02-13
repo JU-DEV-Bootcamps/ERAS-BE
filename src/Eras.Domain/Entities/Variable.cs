d79d2a439b2c6f2693813b7309ba02b62d99eb4d
Ram <ramiro.vargas@jala.university>
2025-02-13 17:29:04 -0400

    feat(domain): added type to variable entity

--- src/Eras.Domain/Entities/Variable.cs
+++ src/Eras.Domain/Entities/Variable.cs
@@ -5,6 +5,7 @@ namespace Eras.Domain.Entities
     public class Variable: BaseEntity, IAuditableEntity
     {
         public string Name { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty;
         public AuditInfo Audit { get; set; } = default!;
     }
 }
