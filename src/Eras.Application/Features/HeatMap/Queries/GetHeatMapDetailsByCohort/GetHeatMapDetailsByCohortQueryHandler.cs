7b69b33598f22b4af8918a9225a40236c6e6b56b
tintayadev <pau.tintaya.code@gmail.com>
2025-03-10 11:55:45 -0400

    feat(heatmap): add cohort-based heatmap query and handler

---
+++ src/Eras.Application/Features/HeatMap/Queries/GetHeatMapDetailsByCohort/GetHeatMapDetailsByCohortQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using Eras.Application.Contracts.Persistence;
+using Eras.Application.DTOs.HeatMap;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Eras.Application.Features.HeatMap.Queries.GetHeatMapDetailsByCohort
+{
+    public class GetHeatMapDetailsByCohortQueryHandler
+        : IRequestHandler<GetHeatMapDetailsByCohortQuery, List<StudentHeatMapDetailDto>>
+    {
+        private readonly ILogger<GetHeatMapDetailsByCohortQueryHandler> _logger;
+        private readonly IStudentRepository _studentRepository;
+
+        public GetHeatMapDetailsByCohortQueryHandler(
+            IStudentRepository studentRepository,
+            ILogger<GetHeatMapDetailsByCohortQueryHandler> logger
+        )
+        {
+            _logger = logger;
+            _studentRepository = studentRepository;
+        }
+
+        public async Task<List<StudentHeatMapDetailDto>> Handle(
+            GetHeatMapDetailsByCohortQuery request,
+            CancellationToken cancellationToken
+        )
+        {
+            _logger.LogDebug($"Heat map details by cohort: {request.CohortId}");
+            return await _studentRepository.GetStudentHeatMapDetailsByCohort(
+                request.CohortId,
+                request.limit
+            );
+        }
+    }
+}
