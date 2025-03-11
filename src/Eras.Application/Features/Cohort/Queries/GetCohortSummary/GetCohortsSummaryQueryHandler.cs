dbfaac6a4202d492be442015df714bd60fa1d2af
Adrian Rojas <42558621+AdrianRojasA@users.noreply.github.com>
2025-03-11 16:28:18 -0400

    Feature/eras115/home (#111)
    * feat: ipdated and created summaries
    
    * Summaries and details for cohorts
    
    * some things to update before merge
    
    * No tests yet
    
    * Silly conflicts
    
    * test: added tests for cohortscontroller
    
    * Deleted not related migrations
    
    * Folder structure test updated
    
    * Removed pending tests
    
    * Update controller for counts
    
    * Update src/Eras.Application/Features/Cohort/Queries/GetCohortSummary/GetCohortsSummaryQueryHandler.cs
    
    format improvement
    
    Co-authored-by: Paulo Tintaya <pau.tintaya.code@gmail.com>
    
    * Update src/Eras.Application/Features/EvaluationProcess/Queries/GetEvaluationProcessSumaryQueryHandler.cs
    
    format improvement
    
    Co-authored-by: Paulo Tintaya <pau.tintaya.code@gmail.com>
    
    * Update src/Eras.Infrastructure/Persistence/PostgreSQL/Repositories/StudentCohortRepository.cs
    
    format improvement
    
    Co-authored-by: Paulo Tintaya <pau.tintaya.code@gmail.com>
    
    ---------
    
    Co-authored-by: Paulo Tintaya <pau.tintaya.code@gmail.com>

---
+++ src/Eras.Application/Features/Cohort/Queries/GetCohortSummary/GetCohortsSummaryQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using Eras.Application.Contracts.Persistence;
+using Eras.Domain.Entities;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Eras.Application.Features.Cohort.Queries
+{
+    class GetCohortsSummaryQueryHandler(
+        IStudentCohortRepository scRepository,
+        ILogger<GetCohortsSummaryQuery> logger)
+        : IRequestHandler<GetCohortsSummaryQuery, List<(Student Student, List<PollInstance> PollInstances)>>
+    {
+        private readonly IStudentCohortRepository _screpository = scRepository;
+        private readonly ILogger<GetCohortsSummaryQuery> _logger = logger;
+
+        public async Task<List<(Student Student, List<PollInstance> PollInstances)>> Handle(
+            GetCohortsSummaryQuery request, 
+            CancellationToken cancellationToken)
+        {
+            var cohortSummary = await _screpository.GetCohortsSummaryAsync();
+            return cohortSummary;
+        }
+    }
+}
