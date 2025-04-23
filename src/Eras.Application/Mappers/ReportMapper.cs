cbf6c93247be38a950d1fb09d1983573457f5d7b
Adrian Rojas <adrian.rojas@jalasoft.com>
2025-04-23 13:53:18 -0400

    avgreport update: endpoint by variable

--- src/Eras.Application/Mappers/ReportMapper.cs
+++ src/Eras.Application/Mappers/ReportMapper.cs
@@ -11,15 +11,15 @@ public static class ReportMapper
         .Select(AnsPerComp => new AvgReportComponent
         {
             Description = AnsPerComp.Key.ToUpper(),
-            AverageRisk = AnsPerComp.Average(Ans => Ans.RiskLevel),
+            AverageRisk = Math.Round(AnsPerComp.Average(Ans => Ans.RiskLevel), 2),
             Questions = [.. AnsPerComp
                 .OrderByDescending(Ans => Ans.RiskLevel)
                 .GroupBy(A => A.Question)
                 .Select(AnsPerVar => new AvgReportQuestions
                 {
                     Question = AnsPerVar.Key,
-                    AverageRisk = AnsPerVar.Average(Ans => Ans.RiskLevel),
-                    Answer = AnsPerVar.First(A => A.RiskLevel <= AnsPerComp.Average(Ans => Ans.RiskLevel)).AnswerText ?? "No answer found.",
+                    AverageRisk = Math.Round(AnsPerVar.Average(Ans => Ans.RiskLevel), 2),
+                    Answer = AnsPerVar.FirstOrDefault(A => A.RiskLevel <= AnsPerComp.Average(Ans => Ans.RiskLevel))?.AnswerText ?? "No answer found for that avg risk level (multiple answer).",
                 })
                 .OrderBy(Q => Q.AverageRisk)]
         })];
