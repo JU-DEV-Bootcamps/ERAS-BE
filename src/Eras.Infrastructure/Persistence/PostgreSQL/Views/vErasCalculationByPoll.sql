fd20a09f8592475aadcfd034725de3ed1de1bd19
Adrian Rojas <42558621+AdrianRojasA@users.noreply.github.com>
2025-04-25 09:07:41 -0400

    Update on the sql view (#149)
    * Updated on the sql view
    
    * whitespaces fixed for lint
    
    * lib order fixed for lint
    
    * whitespaces fixed for lint

--- src/Eras.Infrastructure/Persistence/PostgreSQL/Views/vErasCalculationByPoll.sql
+++ src/Eras.Infrastructure/Persistence/PostgreSQL/Views/vErasCalculationByPoll.sql
@@ -1,56 +1,57 @@
 CREATE OR REPLACE VIEW vErasCalculationByPoll AS
 WITH PercentageCalc AS (
-    SELECT 
+    SELECT
         a.poll_variable_id,
         answer_text,
         ROUND(
-            (COUNT(answer_text) * 100.0) / 
+            (COUNT(answer_text) * 100.0) /
             SUM(COUNT(answer_text)) OVER (PARTITION BY a.poll_variable_id),
             2
-        ) AS percentage,
+        ) AS answer_percentage,
         COUNT(answer_text) AS answer_count
-    FROM 
+    FROM
         answers a
-    GROUP BY 
+    GROUP BY
         a.poll_variable_id, answer_text
 ),
 RiskAverageByComponent AS (
-    SELECT 
+    SELECT
         c."name" AS component_name,
         AVG(a.risk_level) AS average_risk
-    FROM 
+    FROM
         answers a
     JOIN poll_variable pv ON a.poll_variable_id = pv."Id"
     JOIN variables v ON pv.variable_id = v."Id"
     JOIN components c ON v.component_id = c."Id"
-    GROUP BY 
+    GROUP BY
         c."name"
 ),
 RiskAverageByVariable AS (
-    SELECT 
+    SELECT
         v."Id" AS variable_id,
         v."name" AS variable_name,
         AVG(a.risk_level) AS average_risk
-    FROM 
+    FROM
         answers a
     JOIN poll_variable pv ON a.poll_variable_id = pv."Id"
     JOIN variables v ON pv.variable_id = v."Id"
-    GROUP BY 
+    GROUP BY
         v."Id", v."name"
 ),
 RiskCountByPollInstance AS (
-    SELECT 
+    SELECT
         a.poll_instance_id,
         s."name",
-        SUM(a.risk_level) AS risk_count
-    FROM 
+        SUM(a.risk_level) AS poll_instance_risk_sum,
+        COUNT(a.risk_level) AS poll_instance_answers_count
+    FROM
         answers a
-    join poll_instances pi2 on pi2."Id"  = a.poll_instance_id 
+    join poll_instances pi2 on pi2."Id"  = a.poll_instance_id
     join students s on s."Id" = pi2."StudentId"
-    GROUP BY 
+    GROUP BY
         a.poll_instance_id, s."name"
 )
-SELECT 
+SELECT
     p."uuid" AS poll_uuid,
     c."name" AS component_name,
     pc.poll_variable_id,
@@ -58,13 +59,14 @@ SELECT
     pc.answer_text,
     a.poll_instance_id,
     rcbi."name",
-    SUM(a.risk_level) AS risk_sum,
-    rcbi.risk_count,
-    rac.average_risk,
+    a.risk_level AS answer_risk,
+    rcbi.poll_instance_risk_sum,
+    rcbi.poll_instance_answers_count,
+    rac.average_risk AS component_average_risk,
     rav.average_risk AS variable_average_risk,
     pc.answer_count,
-    pc.percentage
-FROM 
+    pc.answer_percentage
+FROM
     answers a
 JOIN poll_variable pv ON a.poll_variable_id = pv."Id"
 JOIN variables v ON pv.variable_id = v."Id"
@@ -74,7 +76,7 @@ JOIN PercentageCalc pc ON a.poll_variable_id = pc.poll_variable_id AND a.answer_
 JOIN RiskAverageByComponent rac ON c."name" = rac.component_name
 JOIN RiskAverageByVariable rav ON v."Id" = rav.variable_id
 JOIN RiskCountByPollInstance rcbi ON a.poll_instance_id = rcbi.poll_instance_id
-GROUP BY 
-    poll_uuid, component_name, pc.poll_variable_id, question, pc.answer_text, a.poll_instance_id,rcbi."name", pc.answer_count, pc.percentage, rcbi.risk_count, rac.average_risk, rav.average_risk, c."name"
-ORDER BY 
-    pc.poll_variable_id, pc.percentage DESC;
+GROUP BY
+    poll_uuid, component_name, pc.poll_variable_id, question, pc.answer_text, a.poll_instance_id, rcbi."name", pc.answer_count, pc.answer_percentage, rcbi.poll_instance_risk_sum, rcbi.poll_instance_answers_count , rac.average_risk, rav.average_risk, c."name", a.risk_level
+ORDER BY
+    pc.poll_variable_id, pc.answer_percentage DESC;
