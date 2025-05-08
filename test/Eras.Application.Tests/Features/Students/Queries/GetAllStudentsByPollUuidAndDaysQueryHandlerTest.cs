e0459188b82647307173fe6837637d2d5dc4fd79
Ramiro Vargas Chavez <Ramiro.Vargas@jalasoft.com>
2025-05-08 15:19:36 -0400

    feat(test): add more unit tests

--- test/Eras.Application.Tests/Features/Students/Queries/GetAllStudentsByPollUuidAndDaysQueryHandlerTest.cs
+++ test/Eras.Application.Tests/Features/Students/Queries/GetAllStudentsByPollUuidAndDaysQueryHandlerTest.cs
@@ -4,7 +4,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Eras.Application.Contracts.Persistence;
+using Eras.Application.Features.Students.Queries.GetAll;
+using Eras.Application.Features.Students.Queries.GetAllByPollAndDate;
+using Eras.Application.Utils;
+using Eras.Domain.Entities;
+using Microsoft.Extensions.Logging;
+using Moq;
+
 namespace Eras.Application.Tests.Features.Students.Queries;
-internal class GetAllStudentsByPollUuidAndDaysQueryHandlerTest
+public class GetAllStudentsByPollUuidAndDaysQueryHandlerTest
 {
+    private readonly Mock<IStudentRepository> _mockStudentRepository;
+    private readonly Mock<ILogger<GetAllStudentsByPollUuidAndDaysQueryHandler>> _mockLogger;
+    private readonly GetAllStudentsByPollUuidAndDaysQueryHandler _handler;
+
+    public GetAllStudentsByPollUuidAndDaysQueryHandlerTest()
+    {
+        _mockStudentRepository = new Mock<IStudentRepository>();
+        _mockLogger = new Mock<ILogger<GetAllStudentsByPollUuidAndDaysQueryHandler>>();
+        _handler = new GetAllStudentsByPollUuidAndDaysQueryHandler(_mockStudentRepository.Object, _mockLogger.Object);
+    }
+
+    [Fact]
+    public async Task Handle_Should_Return_Success_ResponseAsync()
+    {
+        // Arrange
+        var query = new GetAllStudentsByPollUuidAndDaysQuery()
+        {
+            Query = new Utils.Pagination(),
+            PollUuid = "1",
+            Days = 1
+        };
+        List<Student> students = new List<Student>()
+            {
+                new Student(){Email = "StudentEmail1",},
+                new Student(){Email = "StudentEmail2"}
+            };
+        var response = (students,2);
+        _mockStudentRepository
+            .Setup(Repo => Repo.GetAllStudentsByPollUuidAndDaysQuery(It.IsAny<int>(), It.IsAny<int>(),It.IsAny<string>(), It.IsAny<int?>())).ReturnsAsync(response);
+
+        var result = await _handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        Assert.Equal(2, result.Items.Count);
+    }
 }
