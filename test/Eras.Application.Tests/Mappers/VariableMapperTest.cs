00f357a839e89d956926b9458a3b615f6dd68465
Ramiro Vargas Chavez <Ramiro.Vargas@jalasoft.com>
2025-05-08 14:47:27 -0400

    feat(test): add more unit tests

---
+++ test/Eras.Application.Tests/Mappers/VariableMapperTest.cs
@@ -0,0 +1,35 @@
+﻿using Eras.Application.Mappers;
+using Eras.Application.DTOs;
+using Eras.Domain.Entities;
+
+namespace Eras.Application.Tests.Mappers;
+public class VariableMapperTest
+{
+    [Fact]
+    public void ToDomain_Should_Convert_VariableDTO_To_Variable()
+    {
+        var dto = new VariableDTO()
+        { 
+           Name = "Name",
+           Type = "Type",
+        };
+        var result = dto.ToDomain();
+        Assert.NotNull(result);
+        Assert.Equal(dto.Name, result.Name);
+        Assert.Equal(dto.Type, result.Type);
+    }
+
+    [Fact]
+    public void ToDto_Should_Convert_Variable_To_VariableDto()
+    {
+        var model = new Variable()
+        {
+            Name = "Name",
+            Type = "Type",
+        };
+        var result = model.ToDto();
+        Assert.NotNull(result);
+        Assert.Equal(model.Name, result.Name);
+        Assert.Equal(model.Type, result.Type);
+    }
+}
