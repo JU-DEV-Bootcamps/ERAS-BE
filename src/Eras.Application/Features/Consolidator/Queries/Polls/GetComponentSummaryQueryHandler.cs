29e7cf40470ff4c7f63f099a9fad1c6b813b38c6
MauroFonsecaJala <mauro.fonseca@jalasoft.com>
2025-05-14 16:08:25 -0300

    feature(GetComponentSummaryQuery): add query class

---
+++ src/Eras.Application/Features/Consolidator/Queries/Polls/GetComponentSummaryQueryHandler.cs
@@ -0,0 +1,39 @@
+﻿using Eras.Application.Contracts.Persistence;
+using Eras.Application.Models.Response.Common;
+using Eras.Domain.Entities;
+
+using MediatR;
+
+using Microsoft.Extensions.Logging;
+
+namespace Eras.Application.Features.Consolidator.Queries.Polls;
+
+public class GetComponentSummaryQueryHandler(
+ ILogger<GetPollTopQueryHandler> Logger,
+ IPollVariableRepository PollVariableRepository
+) : IRequestHandler<GetComponentSummaryQuery, GetQueryResponse<List<Answer>>>
+{
+    private readonly ILogger<GetPollTopQueryHandler> _logger = Logger;
+    private readonly IPollVariableRepository _pollVariableRepository = PollVariableRepository;
+
+    public async Task<GetQueryResponse<List<Answer>>> Handle(
+        GetComponentSummaryQuery Request,
+        CancellationToken CancellationToken)
+    {
+        try
+        {
+            List<Answer> results = await _pollVariableRepository.GetSummaryByPollUuidAsync(Request.PollUuid.ToString());
+            //var orderedStudents = results.OrderByDescending(Stud => Stud.Answer.RiskLevel).ToList();
+            return new GetQueryResponse<List<Answer>>(results, "Success", true);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "An error occurred while calculating higher risk students: ");
+            return new GetQueryResponse<List<Answer>>(
+                [],
+                $"Failed to retrieve top risk students by variable. Error {e.Message}",
+                false
+            );
+        }
+    }
+}
