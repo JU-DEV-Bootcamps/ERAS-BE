5fbe8d82a4942e762e2ddd933e8cdb610fed9665
Israel Quisbert Anti <Israel.Quisbert@jalasoft.com>
2025-08-14 18:55:35 -0400

    feat(ERAS-381): entities created for remissions and interventions

---
+++ src/Eras.Infrastructure/Persistence/PostgreSQL/Entities/JUInterventionsEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Eras.Domain.Common;
+
+namespace Eras.Infrastructure.Persistence.PostgreSQL.Entities;
+public class JUInterventionsEntity : BaseEntity, IAuditableEntity
+{
+    public int StudentId { get; set; }
+    public required StudentEntity Student { get; set; }
+    public string Diagnostic { get; set; } = string.Empty;
+    public string Objective { get; set; } = string.Empty;
+    public IEnumerable<JURemissionEntity> RemissionsList { get; set; } = [];
+    public required AuditInfo Audit { get; set; }
+}
