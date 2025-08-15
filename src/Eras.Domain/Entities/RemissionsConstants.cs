44b7f4391b5bfc570cbbb55cd0f35feaf53dc1e7
Israel Quisbert Anti <Israel.Quisbert@jalasoft.com>
2025-08-15 11:46:00 -0400

    feat(ERAS-381): implemented configurations and migrations for remissions and interventions

--- src/Eras.Domain/Entities/RemissionsConstants.cs
+++ src/Eras.Domain/Entities/RemissionsConstants.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Eras.Domain.Entities;
-internal class RemissionsConstants
+public static class RemissionsConstants
 {
+    public enum RemissionsStatus
+    {
+        [Description("Remission it's created")]
+        Created,
+        [Description("Remission remitted to a professional")]
+        Remitted,
+        [Description("Remission assigned to a professional but not working on it")]
+        OnHold,
+        [Description("Remission it's in progress by a professional")]
+        InProgress,
+        [Description("Remission it's completed")]
+        Completed
+    }
 }
---
+++ src/Eras.Infrastructure/Persistence/PostgreSQL/Configurations/JUInterventionsConfiguration.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Eras.Infrastructure.Persistence.PostgreSQL.Entities;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Eras.Infrastructure.Persistence.PostgreSQL.Configurations;
+public class JUInterventionsConfiguration : IEntityTypeConfiguration<JUInterventionsEntity>
+{
+    public void Configure(EntityTypeBuilder<JUInterventionsEntity> Builder)
+    {
+        Builder.ToTable("ju_interventions");
+
+        ConfigureColumns(Builder);
+        ConfigureRelationShips(Builder);
+        AuditConfiguration.Configure(Builder);
+    }
+
+    private void ConfigureColumns(EntityTypeBuilder<JUInterventionsEntity> Builder)
+    {
+        Builder.HasKey(i => i.Id);
+
+        Builder.Property(i => i.Diagnostic)
+            .HasColumnName("diagnostic")
+            .HasMaxLength(500);
+
+        Builder.Property(i => i.Objective)
+            .HasColumnName("objective")
+            .HasMaxLength(500);
+
+        Builder.Property(i => i.StudentId)
+            .HasColumnName("student_id")
+            .IsRequired();
+    }
+    private void ConfigureRelationShips(EntityTypeBuilder<JUInterventionsEntity> Builder)
+    {
+        Builder.HasOne(i => i.Student)
+            .WithMany()
+            .HasForeignKey(i => i.StudentId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        Builder.HasMany(i => i.RemissionsList)
+            .WithOne()
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
