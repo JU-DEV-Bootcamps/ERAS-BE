a0e296582dc8db032180e317791bcca377cc565c
Carlos Pammo <Carlos.Pammo@jalasoft.com>
2025-11-18 10:52:20 -0400

    cherry pick for BE-274

--- src/Eras.Application/Features/Evaluations/Commands/CreateEvaluation/CreateEvaluationCommandHandler.cs
+++ src/Eras.Application/Features/Evaluations/Commands/CreateEvaluation/CreateEvaluationCommandHandler.cs
@@ -1,69 +1,73 @@
 ﻿using Eras.Application.Contracts.Persistence;
-using Eras.Application.Mappers;
-using Eras.Application.Models.Response.Common;
-using Eras.Domain.Common;
 using Eras.Domain.Entities;
-using Eras.Error.Bussiness;
-
 using MediatR;
+using Microsoft.Extensions.Logging;
+using Eras.Application.Mappers;
+using Eras.Domain.Common;
+using Eras.Application.Models.Response.Common;
 
 namespace Eras.Application.Features.Evaluations.Commands
 {
     public class CreateEvaluationCommandHandler : IRequestHandler<CreateEvaluationCommand, CreateCommandResponse<Evaluation>>
     {
         private readonly IEvaluationRepository _evaluationRepository;
+        private readonly ILogger<CreateEvaluationCommandHandler> _logger;
         private readonly IPollRepository _pollRepository;
         private readonly IMediator _mediator;
-        public CreateEvaluationCommandHandler(IEvaluationRepository EvaluationRepository,IPollRepository PollRepository, IMediator Mediator)
+        public CreateEvaluationCommandHandler(IEvaluationRepository EvaluationRepository,IPollRepository PollRepository,
+            ILogger<CreateEvaluationCommandHandler> Logger, IMediator Mediator)
         {
             _evaluationRepository = EvaluationRepository;
             _pollRepository = PollRepository;
+            _logger = Logger;
             _mediator = Mediator;
         }
 
         public async Task<CreateCommandResponse<Evaluation>> Handle(CreateEvaluationCommand Request, CancellationToken CancellationToken)
         {
-            string status = EvaluationConstants.EvaluationStatus.Pending.ToString();
-            Evaluation? evaluation=null;
-            Poll? poll = null;
-            evaluation = await _evaluationRepository.GetByNameAsync(Request.EvaluationDTO.Name);
+            try
+            {
+                string status = EvaluationConstants.EvaluationStatus.Pending.ToString();
+                Evaluation? evaluation=null;
+                Poll? poll = null;
+                evaluation = await _evaluationRepository.GetByNameAsync(Request.EvaluationDTO.Name);
 
-            if (evaluation != null)
-            {
-                throw new ExistingEvaluationNameException(Request.EvaluationDTO.Name);
-            }                                        
+                if (evaluation != null) return new CreateCommandResponse<Evaluation>(null, 0,
+                    $"Evaluation with Name {Request.EvaluationDTO.Name} already exists", false);
 
-            if (!Request.EvaluationDTO.PollName.Equals(string.Empty))
-            {
-                poll = await _pollRepository.GetByParentIdAsync(Request.ParentId);
-            }
-
-            evaluation = Request.EvaluationDTO.ToDomain();
-            evaluation.Audit = new AuditInfo()
-            {
-                CreatedBy = "Controller",
-                CreatedAt = DateTime.UtcNow,
-                ModifiedAt = DateTime.UtcNow,
-            };
-            
-            if (poll != null && !Request.EvaluationDTO.PollName.Equals(string.Empty))
-            {
-                evaluation.PollId = poll.Id;
-                status = EvaluationConstants.EvaluationStatus.Ready.ToString();
-            }
-            evaluation.Status = status;
-            Evaluation response = await _evaluationRepository.AddAsync(evaluation);
-            if (poll != null && status.Equals(EvaluationConstants.EvaluationStatus.Ready.ToString()))
-            {
-                Request.EvaluationDTO.PollId = poll.Id;
-                Request.EvaluationDTO.Id = response.Id;
-                CreateEvaluationPollCommand evaluationPollCommand = new()
+                if (!Request.EvaluationDTO.PollName.Equals(string.Empty))
+                    poll = await _pollRepository.GetByParentIdAsync(Request.ParentId);
+                evaluation = Request.EvaluationDTO.ToDomain();
+                evaluation.Audit = new AuditInfo()
                 {
-                    EvaluationDTO = Request.EvaluationDTO
+                    CreatedBy = "Controller",
+                    CreatedAt = DateTime.UtcNow,
+                    ModifiedAt = DateTime.UtcNow,
                 };
-                await _mediator.Send(evaluationPollCommand);
+                if (poll != null && !Request.EvaluationDTO.PollName.Equals(string.Empty))
+                {
+                    evaluation.PollId = poll.Id;
+                    status = EvaluationConstants.EvaluationStatus.Ready.ToString();
+                }
+                evaluation.Status = status;
+                Evaluation response = await _evaluationRepository.AddAsync(evaluation);
+                if (poll != null && status.Equals(EvaluationConstants.EvaluationStatus.Ready.ToString()))
+                {
+                    Request.EvaluationDTO.PollId = poll.Id;
+                    Request.EvaluationDTO.Id = response.Id;
+                    CreateEvaluationPollCommand evaluationPollCommand = new()
+                    {
+                        EvaluationDTO = Request.EvaluationDTO
+                    };
+                    await _mediator.Send(evaluationPollCommand);
+                }
+                return new CreateCommandResponse<Evaluation>(response, 1, "Success", true);
             }
-            return new CreateCommandResponse<Evaluation>(response, 1, "Success", true);
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred creating the evaluation: " + Request.EvaluationDTO.Name);
+                return new CreateCommandResponse<Evaluation>(null, 0, "Error", false);
+            }
         }
     }
 }
