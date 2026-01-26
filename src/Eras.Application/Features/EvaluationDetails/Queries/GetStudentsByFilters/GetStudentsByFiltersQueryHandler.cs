49e54996f96bfc66ee7bf1ba6d38b163ce3f6ddf
Israel Quisbert Anti <Israel.Quisbert@jalasoft.com>
2026-01-26 12:17:05 -0400

    CherryPick_321_update

---
+++ src/Eras.Application/Features/EvaluationDetails/Queries/GetStudentsByFilters/GetStudentsByFiltersQueryHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Eras.Application.Contracts.Persistence;
+using Eras.Application.Features.EvaluationDetails.Queries.GetEvaluationDetailsByFilters;
+using Eras.Application.Models.Response.Controllers.EvaluationDetailsController;
+using Eras.Domain.Entities;
+
+using MediatR;
+
+using Microsoft.Extensions.Logging;
+
+namespace Eras.Application.Features.EvaluationDetails.Queries.GetStudentsByFilters;
+
+public class GetStudentsByFiltersQueryHandler : IRequestHandler<GetStudentsByFiltersQuery, List<StudentsByFiltersResponse>>
+{
+    private readonly IErasEvaluationDetailsViewRepository _repository;
+    private readonly ILogger<GetStudentsByFiltersQueryHandler> _logger;
+
+    public GetStudentsByFiltersQueryHandler(IErasEvaluationDetailsViewRepository Repository, ILogger<GetStudentsByFiltersQueryHandler> Logger)
+    {
+        _repository = Repository;
+        _logger = Logger;
+    }
+
+    public async Task<List<StudentsByFiltersResponse>> Handle(GetStudentsByFiltersQuery Request, CancellationToken CancellationToken)
+    {
+        var studentsList = await _repository.GetStudentsByFilters(
+            Request.PollId,
+            Request.ComponentIds,
+            Request.CohortIds,
+            Request.VariableIds
+        );
+
+        return studentsList;
+    }
+}
