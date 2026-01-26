0dca3931517b8403eae04fca24477b4fb3c1fdd8
Israel Quisbert Anti <Israel.Quisbert@jalasoft.com>
2026-01-26 12:17:05 -0400

    BE-321. Student filtering applied and data model simplified

---
+++ src/Eras.Application/Features/EvaluationDetails/Queries/GetStudentsByFilters/GetStudentsByFiltersQuery.cs
@@ -0,0 +1,14 @@
+﻿using Eras.Application.Models.Response.Controllers.EvaluationDetailsController;
+
+using MediatR;
+
+namespace Eras.Application.Features.EvaluationDetails.Queries.GetStudentsByFilters;
+
+public class GetStudentsByFiltersQuery : IRequest<List<StudentsByFiltersResponse>>
+{
+    public int? PollId { get; set; }
+    public List<int>? ComponentIds { get; set; }
+    public List<int>? CohortIds { get; set; }
+    public List<int>? VariableIds { get; set; }
+
+}
