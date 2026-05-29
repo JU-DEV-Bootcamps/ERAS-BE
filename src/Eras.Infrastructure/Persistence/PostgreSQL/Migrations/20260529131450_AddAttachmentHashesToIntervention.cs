4b121ac2fcd575af8373ecb5493eb43683051069
LuFeBeVi <lucia.becerra@jalasoft.com>
2026-05-29 09:16:21 -0400

    fix: AddAttachmentHashesToIntervention

---
+++ src/Eras.Infrastructure/Persistence/PostgreSQL/Migrations/20260529131450_AddAttachmentHashesToIntervention.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Eras.Infrastructure.Persistence.PostgreSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAttachmentHashesToIntervention : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string[]>(
+                name: "attachment_hashes",
+                table: "Interventions",
+                type: "text[]",
+                nullable: false,
+                defaultValue: new string[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "attachment_hashes",
+                table: "Interventions");
+        }
+    }
+}
