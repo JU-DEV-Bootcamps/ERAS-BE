20f4f41777519523bac1bfa202ca0fcbfe71cafe
Sebastian Beltran <sebastian.beltran@jalasoft.com>
2026-06-17 16:30:23 -0500

    hotfix: increase answer_text max length

---
+++ src/Eras.Infrastructure/Persistence/PostgreSQL/Migrations/20260617202357_UpdateAnswerTextMaxLength.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Eras.Infrastructure.Persistence.PostgreSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateAnswerTextMaxLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "answer_text",
+                table: "answers",
+                type: "character varying(1000)",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(500)",
+                oldMaxLength: 500);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DROP VIEW IF EXISTS vErasCalculationByPoll;");
+            migrationBuilder.Sql(@"DROP VIEW IF EXISTS vErasEvaluationDetails;");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "answer_text",
+                table: "answers",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(1000)",
+                oldMaxLength: 1000);
+            var viewsPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Persistence", "PostgreSQL", "Views" ,"Ups");
+            using StreamReader calcReader = new StreamReader(Path.Combine(viewsPath,"vErasCalculationByPoll.sql"));
+            var calcViewScript = calcReader.ReadToEnd();
+            migrationBuilder.Sql(calcViewScript);
+
+            using StreamReader evalReader = new StreamReader(Path.Combine(viewsPath,"vErasEvaluationDetails.sql"));
+            var evalReaderScript = evalReader.ReadToEnd();
+            migrationBuilder.Sql(evalReaderScript);
+        }
+    }
+}
