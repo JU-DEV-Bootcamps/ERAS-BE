6c518a60368a250c3b24c1a61b3da50d8073a31b
Marcel.Morales <marcel.morales@jalasoft.com>
2026-06-30 18:16:51 -0400

    Adding fix about counter of imported elements

--- src/Eras.Application/Contracts/Persistence/IImportJobRepository.cs
+++ src/Eras.Application/Contracts/Persistence/IImportJobRepository.cs
@@ -4,5 +4,7 @@ namespace Eras.Application.Contracts.Persistence
 {
     public interface IImportJobRepository : IBaseRepository<ImportJob>
     {
+        Task SetStatusAsync(int Id, ImportJobStatus Status, DateTime UpdatedAtUtc);
+        Task SetResultAsync(int Id, ImportJobStatus Status, int ProcessedCount, string? ErrorMessage, DateTime UpdatedAtUtc);
     }
 }
