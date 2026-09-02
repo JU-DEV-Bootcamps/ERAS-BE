39606dab64314b1b7cf2628a3a93b856693b72cf
judithmarg-dev <Judith.Paco@jalasoft.com>
2026-09-02 11:50:25 -0400

    fix: remove unused elements related to juRemissions

--- src/Eras.Application/Contracts/Persistence/IRemissionRepository.cs
+++
@@ -1,9 +0,0 @@
-using Eras.Domain.Entities;
-
-namespace Eras.Application.Contracts.Persistence
-{
-    public interface IRemissionRepository : IBaseRepository<JURemission>
-    {
-        Task<JURemission?> GetBySubmitterUuidAsync(string SubmitterUuid);
-    }
-}
