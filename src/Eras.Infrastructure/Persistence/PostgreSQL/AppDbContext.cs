3c89604eb8871f0afd979622df6c23e2497ebdbc
judithmarg-dev <Judith.Paco@jalasoft.com>
2026-09-15 13:57:28 -0400

    fix: rebase and fix conflicts to keep latest modifications

--- src/Eras.Infrastructure/Persistence/PostgreSQL/AppDbContext.cs
+++ src/Eras.Infrastructure/Persistence/PostgreSQL/AppDbContext.cs
@@ -1,3 +1,4 @@
+using Eras.Domain.Entities;
 using Eras.Domain.Entities.AssessmentManagement;
 using Eras.Domain.Entities.FeatureFlagManagement;
 using Eras.Infrastructure.Persistence.PostgreSQL.Entities;
@@ -29,10 +30,11 @@ namespace Eras.Infrastructure.Persistence.PostgreSQL
         public virtual DbSet<ImportJobEntity> ImportJobs { get; set; }
         public virtual DbSet<ImportJobItemEntity> ImportJobItems { get; set; }
         public virtual DbSet<AttachmentDraftSessionEntity> AttachmentDraftSessions { get; set; }
+        public virtual DbSet<AttachmentEntity> Attachments { get; set; }
+
         public DbSet<Assessment> Assessments => Set<Assessment>();
         public DbSet<Intervention> Interventions => Set<Intervention>();
         public DbSet<FeatureFlag> FeatureFlags => Set<FeatureFlag>();
-        public DbSet<Attachment> Attachments => Set<Attachment>();
         public DbSet<DataMigrationCompletion> DataMigrationCompletions => Set<DataMigrationCompletion>();
 
         // Views
