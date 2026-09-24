5c3cbb248296441c1902ec3d7b2a8eee3b8d5d58
Sebastian Beltran <sebastian.beltran@jalasoft.com>
2026-09-23 19:04:17 -0500

    feat: add migration to use Sub instead of name reference in assessments

---
+++ src/Eras.Infrastructure/Persistence/PostgreSQL/Migrations/20260923160703_UseSubInsteadOfNameRefInAssessments.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Eras.Infrastructure.Persistence.PostgreSQL.Migrations
+{
+    [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+    public partial class UseSubInsteadOfNameRefInAssessments : Migration
+    {
+        /// <summary>
+        /// Looks for a user in eras_users table using the created_by and
+        /// assigned_professional values (full names) and replaces them
+        /// with the user sub. If there is no match, ignores the row.
+        /// </summary>
+        /// <param name="migrationBuilder"></param>
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE remissions r
+                SET created_by = eu.sub
+                FROM eras_users eu
+                WHERE LOWER(TRIM(r.created_by)) = LOWER(TRIM(eu.first_name || ' ' || eu.last_name))
+                    AND r.created_by IS NOT NULL;
+            ");
+
+            migrationBuilder.Sql(@"
+                UPDATE remissions r
+                SET assigned_professional = eu.sub
+                FROM eras_users eu
+                WHERE LOWER(TRIM(r.assigned_professional)) = LOWER(TRIM(eu.first_name || ' ' || eu.last_name))
+                    AND r.assigned_professional IS NOT NULL;
+            ");
+        }
+
+        /// <summary>
+        /// Looks for a user in eras_users table using the created_by and
+        /// assigned_professional values (subs) and replaces them
+        /// with the user full name. If there is no match, ignores the row.
+        /// </summary>
+        /// <param name="migrationBuilder"></param>
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE remissions r
+                SET created_by = eu.first_name || ' ' || eu.last_name
+                FROM eras_users eu
+                WHERE r.created_by = eu.sub
+                    AND r.created_by IS NOT NULL;
+            ");
+
+            migrationBuilder.Sql(@"
+                UPDATE remissions r
+                SET assigned_professional = eu.first_name || ' ' || eu.last_name
+                FROM eras_users eu
+                WHERE r.assigned_professional = eu.sub
+                    AND r.assigned_professional IS NOT NULL;
+            ");
+        }
+    }
+}
